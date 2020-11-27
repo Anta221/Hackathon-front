f2c663bb7cf518687655e49d864523c25c0744f9
nolan <nolan@e-major.fr>
2020-11-27 13:17:41 +0100

    get projet and member to detail

---
+++ src/scripts/project.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', function () {
+    //    init action to load page 
+
+    if (sessionStorage.getItem('token')) {
+        if (typeof sessionStorage.getItem('token') === 'string') {
+            $.ajax({
+                type: 'GET',
+                url: `http://localhost:3000/projects/${getUrlParams('project_id')}`,
+                success: (project) => {
+                    // get project
+                    console.log(project)
+                    $.ajax({
+                        type: 'GET',
+                        url: `http://localhost:3000/projects/${getUrlParams('project_id')}/members`,
+                        success: (members) => {
+                            // get project
+                            console.log(members)
+                        },
+                        error: (err) => {
+                            console.log('error')
+                            // generate error
+                        }
+                    });
+                },
+                error: (err) => {
+                    console.log('error')
+                    // generate error
+                }
+            });
+        }
+    } else {
+        window.location = 'index.html';
+    }
+
+
+});
+
+
+function getUrlParams(param) {
+    const sPageURL = window.location.search.substring(1);
+    const sURLVariables = sPageURL.split('&');
+    for (const i = 0; i < sURLVariables.length; i++) {
+        const sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == param) {
+
+            return sParameterName[1];
+        }
+
+    }
+}
