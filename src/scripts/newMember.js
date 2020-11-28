8746a4c355d5317947a9b22a92f77f9d5a70a205
nolan <nolan@e-major.fr>
2020-11-28 14:07:00 +0100

    recuperation des ecole pour selection un projet

--- src/scripts/newMember.js
+++ src/scripts/newMember.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
     //on récupere la liste des ecoles
-    var tabSchool = []
+    // var tabSchool = []
     $.get({
         url: 'http://localhost:3000/schools',
-        success: (response) => {
-            for(let i = 0; i < response.length; i++){
-                const element = response[i]
-                tabSchool.push(element._id)
-            }
+        success: (schools) => {
+            // for(let i = 0; i < response.length; i++){
+            //     const element = response[i]
+            //     tabSchool.push(element._id)
+            // }
+
+            generateListSchools(schools);
 
         },
         error: (error) => {
@@ -15,8 +17,6 @@ $(document).ready(function(){
 
         }
        }) 
-    console.log(tabSchool)
-
 
 
     //on récupere la liste des projets
@@ -36,7 +36,67 @@ $(document).ready(function(){
         url: 'http://localhost:3000/schools/:school_id/projects',
 
     ) */
-})
+});
+
+function generateListSchools(schools) {
+    let baseHtml = document.getElementById('list-schools');
+    if(schools.length > 0) {
+        let label = document.createElement('label');
+        label.innerHTML = 'Listes des écoles';
+        let select = document.createElement('select');
+        select.className = 'form-control';
+        select.name = 'school_id';
+        select.id = 'select_school';
+        select.value = 'default';
+        let optionDefault = document.createElement('option');
+        optionDefault.value = "default";
+        optionDefault.innerHTML = 'Sélectioner une école'
+        select.appendChild(optionDefault);
+        baseHtml.appendChild(label);
+        baseHtml.appendChild(select);
+
+        schools.forEach((school) => {
+            let option = document.createElement('option');
+            option.value = school._id;
+            option.innerHTML = `${school.name} ${school.location}`;
+            select.appendChild(option);
+        })
+    }
+    else
+     {
+        let div = document.createElement('div');
+        div.innerHTML = 'Aucune école de disponible';
+        div.fontWeight = 'bold';
+        baseHtml.appendChild(div);
+    }
+
+ 
+
+
+//on recupére la liste des écoles
+$.get({
+    url: 'http://localhost:3000/schools',
+    success: (response) => {
+        // get projects and show this in html select
+        var htmlTab =[];
+        for (let i = 0; i < response.length; i++) {
+            const element = response[i];
+            let html = "";
+                html+='<option>'+element.name+'</option>'
+
+            htmlTab.push(html)
+            nameId = element._id + ":"+ element.name
+            schoolTab.push(nameId)
+        }
+        
+        $("#select").html(htmlTab);
+    },
+    error: (err) => {
+        console.log(err)
+    }
+});
+}
+
 
 
 
